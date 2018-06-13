37a52b656b3a19b0c3faf42582292760982e6ba6
LPelc <laurence.pelc@hotmail.fr>
2018-06-13 16:05:26 +0200

    Fix view error

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -7,13 +7,21 @@ use Symfony\Component\HttpFoundation\Response;
 
 class SearchController extends FOSRestController
 {
+
     public function index()
     {
         $number = random_int(0, 100);
 
-        return $this->render('search/index.html.twig', array(
+        /*return $this->render('search/index.html.twig', array(
             'number' => $number,
-        ));
+        ));*/
+
+        $view = $this->view($number, 200)
+            ->setTemplate("search/index.html.twig")
+            ->setTemplateVar('number')
+        ;
+
+        return $this->handleView($view);
 
     }
 }
