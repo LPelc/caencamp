f040d6555a48e79a96bde7bcd0f6692dbec67a30
LPelc <laurence.pelc@hotmail.fr>
2018-06-20 17:51:19 +0200

    Add analyzer and get parameter

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -3,16 +3,23 @@
 namespace App\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use FOS\ElasticaBundle\Finder\FinderInterface;
 
 class SearchController extends FOSRestController
 {
+    private $finder;
 
-    public function index()
+    public function __construct(FinderInterface $finder)
     {
-        $number = random_int(0, 100);
+        $this->finder = $finder;
+    }
 
-        $view = $this->view($number, 200)
+    public function index(Request $request)
+    {
+        $result = $this->finder->find($request->query->get('query'));
+        $view = $this->view($result, 200)
             ->setTemplate("search/index.html.twig")
             ->setTemplateVar('number')
         ;
